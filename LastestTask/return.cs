c51e49e46eb6a16bf6a4c24be44a0f74660de484
orxansharifov <sharifovorxan1996@gmail.com>
2022-06-17 21:53:19 +0400

    comment

---
+++ LastestTask/return.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Les7task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum1(5));
+            Console.WriteLine(Sum2(2));
+            Console.WriteLine(Sum3("Text1"));
+            Console.WriteLine(Sum4(15, "Salam"));
+            Console.WriteLine(Sum5(2, 3));
+            Console.WriteLine(Sum6("text2", 44));
+            Console.WriteLine(Sum7("text3", 66));
+            Console.WriteLine(Sum8('e', 222));
+            Console.WriteLine(Sum9(22, 34));
+            Console.WriteLine(sum10(22, 33));
+            Console.WriteLine(sum11(11, 22));
+         
+        }
+        #region
+
+        public static int Sum1(int a)
+        {
+            return a;
+        }
+        public static byte Sum2(long b)
+        {
+            return 11;
+        }
+        public static string Sum3(string c)
+        {
+            return c;
+        }
+        public static int Sum4(long a1, string a2)
+        {
+            return 100;
+        }
+        public static char Sum5(int a3, long a4)
+        {
+            return 's';
+        }
+        public static sbyte Sum6(string a5, long a6)
+        {
+            return 44;
+        }
+        #endregion
+        public static bool Sum7(string a8, long a9)
+        {
+            return true;
+        }
+        public static sbyte Sum8(char b1, long b2)
+        {
+            return 22;
+        }
+        public static int Sum9(sbyte b3, long b4)
+        {
+            return 22;
+        }
+        public static bool sum10(sbyte b5, long b6)
+        {
+            return false;
+        }
+        public static string sum11(byte b7, long b8)
+        {
+            return "tex4";
+        }
+        
+        }
+        
+
+
+
+
+
+
+}
+
+}
---
+++ LastestTask/void methods/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace void_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Num(5);
+            Num2(2);
+            Num3("hello");
+            Num4(44, " Hy");
+            Num5(4, 5);
+            Num6("22",44);
+            Num7(3,3);
+            Num8(44,55);
+            Num9("hy",33);
+            
+        }
+
+        #region
+
+        public static void Num(int a)
+        {
+            Console.WriteLine(a);
+        }
+        public static void Num2(long b)
+        {
+            Console.WriteLine(b);
+        }
+        public static void Num3(string c)
+        {
+            Console.WriteLine(c + 5);
+        }
+        public static void Num4(long a1, string a2)
+        {
+            Console.WriteLine(a1 + a2);
+        }
+        public static void Num5(int a3, long a4)
+        {
+            Console.WriteLine(a3 + a4);
+        }
+        #endregion
+        public static void Num6 (string q1, long q2)
+            {
+            Console.WriteLine("salam",22);
+            }
+        public static void Num7(sbyte q3,long q4)
+        {
+            Console.WriteLine(44);
+        }
+        public static  void Num8(byte t,long t1)
+        {
+            Console.WriteLine(t);
+        }
+        public static void Num9(string qq,long yy)
+        {
+            Console.WriteLine();
+        }
+
+
+}
+}
